80ed24b370fc42759e3e029eb80453c3f2a1902e
Nele Albers <nele.96@web.de>
2020-02-19 15:52:24 +0100

    adapted hallway2 for belief comp

--- gym_pomdp_domains/envs/hallway2.py
+++ gym_pomdp_domains/envs/hallway2.py
@@ -5,7 +5,6 @@
     Actions: stay, forward, turn right, turn left, turn around
     Orientations: up, right, down, left
 '''
-
 import gym
 from gym import spaces
 import numpy as np
@@ -38,6 +37,16 @@ class Hallway2(gym.Env):
     self.TERMINAL_STATES = [68]
     self.NON_TERMINAL_STATES = np.arange(0, self.state_space.n)
     self.NON_TERMINAL_STATES = [i for i in self.NON_TERMINAL_STATES if i not in self.TERMINAL_STATES]
+    
+    # start state
+    self.state = choice(self.NON_TERMINAL_STATES)
+    
+    # optimal actions per state (5 signals the goal state where nothing is optimal)
+    self.OPTIMAL_ACTIONS = [2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3, 4, 4, 2, 1, 3,
+                            2, 1, 3, 4, 1, 23, 1, 23, 1, 23, 1, 23, 4, 2, 1, 3, 3, 4, 2, 1,
+                            4, 2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3,
+                            2, 1, 3, 4, 4, 2, 1, 3, 4, 2, 1, 3, 2, 1, 3, 4, 5,
+                            2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3, 4, 1, 3, 4, 2]
     
     # Walls: in front, to right, behind, to left
     self.WALLS = np.zeros((self.state_space.n, 4))
@@ -100,24 +109,34 @@ class Hallway2(gym.Env):
                           self.state_space.n,
                           self.state_space.n))
     
-    # start state
-    self.state = choice(self.NON_TERMINAL_STATES)
+    # observation matrix
+    self.O = np.zeros((self.action_space.n,
+                          self.state_space.n,
+                          self.observation_space.n))
     
     # any action taken in terminal state has no effect
     for i in self.TERMINAL_STATES:
         self.P[:, i, i] = 1
+        self.O[:, i] = self.get_observation_probabilities(i)
     
-    # transition probabilities
+    # transition and observation probabilities
     for action in range(self.action_space.n):
         for s in self.NON_TERMINAL_STATES:
             s_prime = self.act(s, action)
+            
             if action != 0:
+                # action succeeds
                 self.P[action, s, int(s_prime)] = self.PROB_ACTION_SUCCESS
+                self.O[action, s] = self.PROB_ACTION_SUCCESS * self.get_observation_probabilities(s_prime)
+                
+                # action does not succeed
                 for action_other in [i for i in range(self.action_space.n) if not i == action]:
-                    s_prime = self.act(s, action_other)
-                    self.P[action_other, s, int(s_prime)] = (1 - self.PROB_ACTION_SUCCESS)/4
+                    s_prime_other = self.act(s, action_other)
+                    self.P[action_other, s, int(s_prime_other)] += (1 - self.PROB_ACTION_SUCCESS)/4
+                    self.O[action, s] += self.get_observation_probabilities(s_prime_other) * (1 - self.PROB_ACTION_SUCCESS)/4
             else:
                 self.P[action, s, int(s_prime)] = 1
+                self.O[action, s] = self.get_observation_probabilities(s_prime)
     
     # rewards are given for arriving in a certain state
     self.R = np.full((self.state_space.n), 0)
@@ -129,7 +148,6 @@ class Hallway2(gym.Env):
     '''
     Returns observation for being in state s_prime.
     '''
-    
     s_prime = int(s_prime)
     
     # goal state
@@ -143,13 +161,40 @@ class Hallway2(gym.Env):
             obs[wall_pos] = 1
         elif self.WALLS[s_prime][wall_pos] == 1 and prob < self.PROB_SEE_WALL_TRUE:
             obs[wall_pos] = 1
+            
+    return self.encode_observation(obs)
+
+  def get_observation_probabilities(self, s):
+    '''
+    Computes observation probabilities for arriving in state s.
+    '''
+    probs = np.zeros(self.observation_space.n)
+    true_obs = self.WALLS[s]
+    
+    wall_probs = [[1 - self.PROB_SEE_WALL_FALSE, self.PROB_SEE_WALL_FALSE],
+                  [1 - self.PROB_SEE_WALL_TRUE, self.PROB_SEE_WALL_TRUE]]
+    
+    if s == 68: # goal state
+        probs[16] = 1
+    else:
+        for obs in [[i, j, k, l] for i in range(2) for j in range(2) for k in range(2) for l in range(2)]:
+            p = 1
+            for w in range(4):
+                p *= wall_probs[int(true_obs[w])][int(obs[w])]
+            probs[self.encode_observation(obs)] = p
+     
+    return probs
+    
+  def encode_observation(self, obs):
+    '''
+    Encodes observation of walls to an integer.
+    '''
     return int(obs[0] * 1 + obs[1] * 2 + obs[2] * 4 + obs[3] * 8)
         
   def act(self, s, action):
     '''
     Deterministic next state for taking an action in state s.
     '''
-    
     # actions have no effect in terminal state
     if s in self.TERMINAL_STATES:
         return s
