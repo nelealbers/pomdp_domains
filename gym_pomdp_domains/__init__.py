00ab4cae0b13d133ceedaa78f7f80a4f07b82e2c
nelealbers <43779515+nelealbers@users.noreply.github.com>
2020-05-21 21:27:43 +0200

    Add files via upload

--- gym_pomdp_domains/__init__.py
+++ gym_pomdp_domains/__init__.py
@@ -6,6 +6,11 @@ register(
 )
 
 register(
+    id='Hallway_Simple-v0',
+    entry_point='gym_pomdp_domains.envs:Hallway_Simple',
+)
+
+register(
     id='Hallway2-v0',
     entry_point='gym_pomdp_domains.envs:Hallway2',
 )
